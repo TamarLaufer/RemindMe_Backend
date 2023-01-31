bf1b1fe62de4d747b4fb285fa958ee0d306fb192
Tamar Laufer <40568358+TamarLaufer@users.noreply.github.com>
2023-01-31 18:24:24 +0200

    change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 mongoose.set("strictQuery", true);
 
 app.use(bodyParser.urlencoded({ extended: false }));
